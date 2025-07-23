f40745a6b4e0ac885a772b435a584743d1682496
kwame <kamensgh@gmail.com>
2025-07-23 16:58:04 +0100

    feat: add scroll-linked animation bar and update .gitignore for yarn and Next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,30 @@
 
+
+"use client"
+
 import Header from '../components/header/Header';
 import Content from '../components/content/Content';
 import Footer from '../components/footer/Footer';
+import { motion, useScroll } from "motion/react";
 
 export default function Home() {
+  const { scrollYProgress } = useScroll();
   return (
     <>
+      <motion.div
+        id="scroll-indicator"
+        style={{
+          scaleX: scrollYProgress,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          height: 4,
+          originX: 0,
+          backgroundColor: "#ffc107",
+          zIndex: 9999,
+        }}
+      />
       <Header />
       <Content />
       <Footer />
